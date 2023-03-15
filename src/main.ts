9f6f4e3accbe2b00f140a9ede34ed4c02e6d5628
lasting <lastingcoder@qq.com>
2023-03-15 16:24:39 +0800

    fixed: remove dependency cookie parse

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
 
   const options = new DocumentBuilder()
     .setTitle('student system backend api document')
