e8430fecb90b6adee298ab0a2c0fe09bbed5dcbb
lasting <lastingcoder@qq.com>
2023-04-26 22:43:44 +0800

    refactor: eslint

--- src/modules/requestPost/vo/findAllRequestPostTheStudentSubmit.vo.ts
+++ src/modules/requestPost/vo/findAllRequestPostTheStudentSubmit.vo.ts
@@ -1,4 +1,3 @@
-import { UserResponseVo } from 'src/modules/user/vo/userResponse.vo';
 import { RequestPostVo } from './requestPost.vo';
 import { PostWithCreatedUserVo } from 'src/modules/post/vo/postWithCreatedUser.vo';
 import { ApiProperty } from '@nestjs/swagger';
